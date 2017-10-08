6bb60d3e1a58afe91672311803e4986157d4f804
Pawel Rusin <pawel-rusin@cookpad.com>
2017-10-08 13:02:50 +0900

    Initial Commit

---
+++ TradingGame/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  TradingGame
+//
+//  Created by pawel-rusin on 2017/10/08.
+//  Copyright © 2017 pawel-rusin. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
